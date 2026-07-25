ad53972a6f6820dab0913ed2170924fdeff8bf84
SodiqovSardor <yachiter1112@gmail.com>
2026-07-25 18:57:14 +0500

    pochti carts

--- src/componentes/AdminPanel/Carts.jsx
+++ src/componentes/AdminPanel/Carts.jsx
@@ -1,4 +1,30 @@
+import { useState, useEffect } from "react";
+
 const Carts = () => {
+  const [loading, setLoading] = useState(true);
+  const [carts, setCarts] = useState([]);
+  const api = "https://fakestoreapi.com/carts";
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(api)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setCarts(data);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return (
+      <div>
+        <h1 className="text-2xl font-bold text-gray-900 mb-6">Carts</h1>
+        <p className="text-gray-500">Loading carts...</p>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h1 className="text-2xl font-bold text-gray-900 mb-6">Carts</h1>
